c11a22bbe39cb8662cbd2be7ee8dabf03668250d
Jeremy Bukofzer <jlbukofzer@gmail.com>
2023-01-12 14:45:24 -0600

    Adds index.js to models

--- models/Users.js
+++ models/Users.js
@@ -33,6 +33,10 @@ const userSchema = new Schema (
     }
 )
 
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
 const Users = model('Users', userSchema);
 
 module.exports = Users
